cd3afd4beabf25ac416ed5ea0900bfefc0d35637
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2023-10-25 19:30:19 +0530

    Time: 10 ms (11.83%), Space: 14.3 MB (51.10%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    bool search(vector<int>& arr, int k) {
-       int n = arr.size(); // size of the array.
-    int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        //if mid points the target
-        if (arr[mid] == k) return true;
-
-        //Edge case:
-        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
-            low = low + 1;
-            high = high - 1;
-            continue;
-        }
-
-        //if left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= k && k <= arr[mid]) {
-                //element exists:
-                high = mid - 1;
+    bool search(vector<int>& arr, int target) {
+          
+        int n=arr.size();
+        
+        int low=0, high=n-1, mid;
+        
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+              if(arr[mid]==target) return true;
+            if(arr[low]==arr[mid] and arr[mid]==arr[high]){
+                low+=1;
+                high-=1;
+                continue;
             }
-            else {
-                //element does not exist:
-                low = mid + 1;
+             
+            
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=target and target<=arr[mid]){
+                    high=mid-1;
+                }
+                else
+                    low=mid+1;
             }
-        }
-        else { //if right part is sorted:
-            if (arr[mid] <= k && k <= arr[high]) {
-                //element exists:
-                low = mid + 1;
-            }
-            else {
-                //element does not exist:
-                high = mid - 1;
+            else{
+                if(arr[mid]<=target and target<=arr[high])
+                    low=mid+1;
+                else
+                    high=mid-1;
             }
         }
-    }
-    return false;  
+      
+        return false;
+    
     }
 };
