f629c5cf539637591fda526e517e86df0ad2341a
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2023-11-09 15:47:46 +0530

    Time: 7 ms (97.58%), Space: 10.7 MB (74.49%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,53 +1,59 @@
 class Solution {
 public:
-    
-   int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        
-        
+    int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        
         int m=matrix[0].size();
         
-        vector<vector<int>>dp(n,vector<int>(m,0));
+        if(n==1) return *min_element(matrix[0].begin(),matrix[0].end());
         
-        
-        for(int j=0;j<m;j++)
-        {
-            dp[0][j]=matrix[0][j];
-            
-        }
-        
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                int ld=1e9,rd=1e9;
-                
-                
-                int up=matrix[i][j] + dp[i-1][j];
-                
-                if(j-1>=0)
-                    ld=matrix[i][j] + dp[i-1][j-1];
-                if(j+1<m)
-                    rd=matrix[i][j] + dp[i-1][j+1];
-                
-                dp[i][j] = min(up,min(ld,rd));
-                
-                
-                
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j==0){
+                    matrix[i][j]=matrix[i][j]+min(matrix[i-1][j],matrix[i-1][j+1]);
+                }
+                else if(j==m-1){
+                    matrix[i][j]=matrix[i][j]+min(matrix[i-1][j],matrix[i-1][j-1]);
+                }
+                else{
+                    matrix[i][j]=matrix[i][j]+min(matrix[i-1][j],min(matrix[i-1][j+1],matrix[i-1][j-1]));
+                }
             }
         }
-        
-        int mini=dp[n-1][0];
-        
-        for(int j=1;j<m;j++)
-        {
-            
-            mini=min(mini,dp[n-1][j]);
-            
-        }
-        return mini;
-        
+        return *min_element(matrix[n-1].begin(),matrix[n-1].end());
     }
 };
+
+
+// class Solution {
+// public:
+    
+//     int sol(int i, int j,int n,vector<vector<int>>&matrix,vector<vector<int>>&dp ){
+       
+//         if(j<0 || j>=n) return 1e9;
+        
+//         if(i==0) return matrix[0][j];
+//          if(dp[i][j]!=-1) 
+//             return dp[i][j];
+        
+//         int up=matrix[i][j]+sol(i-1,j,n,matrix,dp);
+//         int leftTop=matrix[i][j]+sol(i-1,j-1,n,matrix,dp);
+//         int rightTop=matrix[i][j]+sol(i-1,j+1,n,matrix,dp);
+        
+//         return dp[i][j]=min(up,(min(leftTop,rightTop)));
+//     }
+    
+   
+    
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//        int n=matrix.size();
+        
+//         vector<vector<int>>dp(n,vector<int>(n,-1));
+        
+//         int result=INT_MAX;
+//         for(int k=0;k<n;k++){
+//             int g=sol(n-1,k,n,matrix,dp);
+//            result=min(result,g) ;
+//         }
+//         return result;
+//     }
+// };
