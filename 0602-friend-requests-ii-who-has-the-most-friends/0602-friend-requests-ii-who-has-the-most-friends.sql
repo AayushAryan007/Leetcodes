4258f778fdef5a3a7d12c4228779a95d6648158b
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2024-01-09 21:34:43 +0530

    Time: 782 ms (11.14%), Space: 0B (100.00%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,8 @@
+select temp.id, count(*) as num from (
+    select requester_id as id from RequestAccepted
+    union all
+    select accepter_id as id from RequestAccepted
+) temp 
+group by temp.id
+order  by num desc
+limit 1
