06254ace582b5f19e047d08f53777e881fe110c4
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2024-02-03 21:30:26 +0530

    Time: 19 ms (49.31%), Space: 21.6 MB (18.70%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,31 +1,58 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int j=-1;
+        int i;
         int n=nums.size();
-        
-        int i=0; int j=0;
-        
-        while(j<n){
-            if(nums[j]!=0){
-                swap(nums[i],nums[j]);
-                i++;
+        for(i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
             }
-            j++;
         }
+        if(j==-1) return ;
         
-//         while(i<n and j<n){
-//              while(i<n and nums[i]==0){
-//             i++;
-//         }
-//         while(j<n and nums[j]!=0){
+        for( i=j+1;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                //increment j and i increments by 2 in loop
+              j++;
+            
+            
+            }   
+            
+        }
+    }
+};
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int n=nums.size();
+        
+//         int i=0; int j=0;
+        
+//         while(j<n){
+//             if(nums[j]!=0){
+//                 swap(nums[i],nums[j]);
+//                 i++;
+//             }
 //             j++;
 //         }
         
-//         if(i<n and j<i){
-//             swap(nums[i],nums[j]);
-//             i++; j++;
-//         }
-//         }
+// //         while(i<n and j<n){
+// //              while(i<n and nums[i]==0){
+// //             i++;
+// //         }
+// //         while(j<n and nums[j]!=0){
+// //             j++;
+// //         }
+        
+// //         if(i<n and j<i){
+// //             swap(nums[i],nums[j]);
+// //             i++; j++;
+// //         }
+// //         }
        
-    }
-};
+//     }
+// };
