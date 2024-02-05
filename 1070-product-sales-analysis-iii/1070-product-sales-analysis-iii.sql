7ad49b1e1c3d3468ca17fb97e2c96366bc6f17e5
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2024-02-05 22:53:13 +0530

    Time: 3231 ms (5.58%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select product_id, year first_year, quantity, price
+from (
+    select *, 
+        rank() over (partition by product_id order by year) rk 
+    from Sales
+) t
+where rk = 1
