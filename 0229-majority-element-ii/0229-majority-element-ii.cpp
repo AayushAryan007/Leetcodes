3ed287e7cdac4f2baec15b007d7c8d73e9d49067
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2024-03-04 18:38:43 +0530

    Time: 14 ms (17.36%), Space: 18.3 MB (54.83%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        map<int,int> mp;
+    vector<int> majorityElement(vector<int>& v) {
+        int n=v.size();
         
-        int n=nums.size();
+        int c1=0, c2=0, e1=INT_MIN,e2 = INT_MIN;
         
-        int calc= int(n/3)+1;
-        vector<int> ls;
+        
         for(int i=0;i<n;i++){
-            
-            mp[nums[i]]++;
-            if(mp[nums[i]]==calc){
-                ls.push_back(nums[i]);
+            if(c1==0 and v[i]!=e2){
+                c1=1;
+                e1=v[i];
             }
-            if(ls.size()==2) break;
+            else if(c2==0 and v[i]!=e1){
+                c2=1;
+                e2=v[i];
+            }
+            else if(v[i]==e1) c1++;
+            else if(v[i]==e2) c2++;
+            else {
+                 c1--,c2--;
+            }
         }
-        return ls;
+        
+        
+        vector<int> ls;
+        c1=0,c2=0;
+        
+        for(int i=0;i<n;i++){
+            if(v[i]==e1) c1++;
+            if(v[i]==e2) c2++;
+        }
+        
+        
+        int mini=int(n/3)+1;
+        if(c1>=mini) ls.push_back(e1);
+        if(c2>=mini) ls.push_back(e2);
+  
+        
+          return ls;
     }
+  
 };
