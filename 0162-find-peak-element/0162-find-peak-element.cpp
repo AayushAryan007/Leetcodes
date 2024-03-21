0ee27aa59de447ddce269a2bc0583102d97cbb9a
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2024-03-21 21:27:34 +0530

    Time: 7 ms (13.19%), Space: 11.2 MB (82.41%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        int l = 0, h = n - 1;
+        int n=nums.size();
         
-        // Handle edge case when array size is 1
-        if (n == 1) {
+        if(n==1){
             return 0;
         }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2]) return n-1;
+        
+        
+        
+        int l=1, h=n-2;
         
         int mid;
-        while (l <= h) {
-            mid = l + (h - l) / 2;
+        while(l<=h){
+            mid=(l+h)/2;
             
-            // Check if mid is a peak element
-            if ((mid == 0 || nums[mid - 1] < nums[mid]) && (mid == n - 1 || nums[mid] > nums[mid + 1])) {
+            if(nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]){
                 return mid;
             }
             
-            // If nums[mid] is not a peak, move towards the peak
-            if (mid > 0 && nums[mid - 1] > nums[mid]) {
-                h = mid - 1;
-            } else {
-                l = mid + 1;
+            if(nums[mid]>nums[mid-1]){
+                l=mid+1;
+                
+            }
+            else{
+                h=mid-1;
             }
         }
-        
-        // This line should never be reached if a peak element exists
         return -1;
     }
 };
