1a2cb2adb5accfba8a10279418e9262677c36045
Aayush Aryan <94699736+AayushAryan007@users.noreply.github.com>
2024-03-21 21:22:22 +0530

    Time: 7 ms (13.19%), Space: 11.3 MB (45.34%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,68 +1,32 @@
-
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)
+        int l = 0, h = n - 1;
+        
+        // Handle edge case when array size is 1
+        if (n == 1) {
             return 0;
-
-        int low = 1;
-        int high = n-2;
-
-        if(nums[0] > nums[1])
-            return 0;
-
-        if(nums[n-1] > nums[n-2])
-            return n-1;
-
-        while(low <= high) {
-            int mid = low + (high-low)/2;
-
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
-                return mid;
-
+        }
+        
+        int mid;
+        while (l <= h) {
+            mid = l + (h - l) / 2;
             
-            else if(nums[mid] > nums[mid-1]) {
-                low = mid+1;
+            // Check if mid is a peak element
+            if ((mid == 0 || nums[mid - 1] < nums[mid]) && (mid == n - 1 || nums[mid] > nums[mid + 1])) {
+                return mid;
             }
-
-            else if(nums[mid-1] > nums[mid]) {
-                high = mid-1;
+            
+            // If nums[mid] is not a peak, move towards the peak
+            if (mid > 0 && nums[mid - 1] > nums[mid]) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
-
+        
+        // This line should never be reached if a peak element exists
         return -1;
     }
 };
-// class Solution {
-// public:
-//     int findPeakElement(vector<int>& nums) {
-        
-//         int n=nums.size();
-        
-//         int s=1, e=n-2;
-//         if(nums[0]>nums[1])
-//             return 0;
-//         if(nums[n-1]>nums[n-2])
-//             return n-1;
-        
-        
-//         while(s<=e){
-            
-//             int mid=(s+e)/2;
-            
-//             if(nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1]){
-//                 return mid;
-//             }  
-//              else if(nums[mid] > nums[mid-1]) {
-//                 s = mid+1;
-//             }
-
-//             else if(nums[mid-1] > nums[mid]) {
-//                 e = mid-1;
-//             }
-//             }
-    
-//         return -1;
-//     }
-// };
